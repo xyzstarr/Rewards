03f535b307c67574eb2b85967776991a0ec4be0e
xyzstarr <thatomantai@gmail.com>
2015-04-13 17:26:11 +0200

    Going Black Signed-off-by:xyzstarr <thatomantai@gmail.com>

--- BMW_Colleague_Rewards/www/app.core/js/app.config.js
+++ BMW_Colleague_Rewards/www/app.core/js/app.config.js
@@ -1,88 +1,88 @@
-(function () {
+(function(){
     "use strict";
     angular
-        .module('app')
-        .config(['$stateProvider', '$urlRouterProvider', fnConfigApp])
-    ;
+            .module('app')
+            .config(['$stateProvider','$urlRouterProvider',fnConfigApp])
+            ;
     //<editor-fold desc="Description">
-    function fnConfigApp($stateProvider, $urlRouterProvider) {
+    function fnConfigApp($stateProvider,$urlRouterProvider){
         $stateProvider
-            .state('app', {
-                url: '/app',
-                templateUrl: "app.core/common/app.tabs.html",
-                abstract: true,
-                controller: 'AppMainController'
-            })
-            .state('app.home', {
-                url: '/home',
-                authenticate: true,
-                views: {
-                    'app-default-view': {
-                        templateUrl: 'app.core/common/app.home.html',
-                        controller: 'AppMainController'
+                .state('app',{
+                    url: '/app',
+                    templateUrl: "app.core/common/app.tabs.html",
+                    abstract: true,
+                    controller: 'AppMainController'
+                })
+                .state('app.home',{
+                    url: '/home',
+                    authenticate: true,
+                    views: {
+                        'app-main': {
+                            templateUrl: 'app.core/common/app.home.html',
+                            controller: 'AppMainController'
+                        }
                     }
-                }
-            })
-            .state('app.login', {
-                url: '/login',
-                authenticate: false,
-                views: {
-                    'app-default-view': {
-                        templateUrl: 'users/auth/app.login.html',
-                        controller: 'AuthController'
+                })
+                .state('app.login',{
+                    url: '/login',
+                    authenticate: false,
+                    views: {
+                        'app-main': {
+                            templateUrl: 'users/auth/app.login.html',
+                            controller: 'AuthController'
+                        }
                     }
-                }
-            })
-            .state('app.signup', {
-                url: '/signup',
-                authenticate: false,
-                views: {
-                    'app-default-view': {
-                        templateUrl: 'users/auth/app.signup.html',
-                        controller: 'AuthController'
+                })
+                .state('app.signup',{
+                    url: '/signup',
+                    authenticate: false,
+                    views: {
+                        'app-main': {
+                            templateUrl: 'users/auth/app.signup.html',
+                            controller: 'AuthController'
+                        }
                     }
-                }
-            })
-            .state('app.colleagues', {
-                url: '/colleagues',
-                authenticate: true,
-                views: {
-                    'app-colleagues-view': {
-                        templateUrl: 'colleagues/partials/app.colleagues.list.html',
-                        controller: 'ColleagueListController'
+                })
+                .state('app.colleagues',{
+                    url: '/colleagues',
+                    authenticate: true,
+                    views: {
+                        'app-colleagues-view': {
+                            templateUrl: 'colleagues/partials/app.colleagues.list.html',
+                            controller: 'ColleagueListController'
+                        }
                     }
-                }
 
-            })
-            .state('app.colleague', {
-                url: '/colleagues/:user_id',
-                authenticate: true,
-                views: {
-                    'app-colleagues-view': {
-                        templateUrl: 'colleague/partials/app.colleague.detail.html',
-                        controller: 'ColleagueController'
+                })
+                .state('app.colleague',{
+                    url: '/colleagues/:user_id',
+                    authenticate: true,
+                    views: {
+                        'app-colleagues-view': {
+                            templateUrl: 'colleague/partials/app.colleague.detail.html',
+                            controller: 'ColleagueController'
+                        }
                     }
-                }
 
-            })
-            .state('app.leaderboard', {
-                url: '/leaderboard',
-                authenticate: true,
-                views: {
-                    'app-colleagues-view': {
-                        templateUrl: 'leaderboard/partials/app.leaderboard.html',
-                        controller: 'LeaderboardController'
+                })
+                .state('app.leaderboard',{
+                    url: '/leaderboard',
+                    authenticate: true,
+                    views: {
+                        'app-leaderboard-view': {
+                            templateUrl: 'leaderboard/partials/app.leaderboard.html',
+                            controller: 'LeaderboardController'
+                        }
                     }
-                }
 
-            })
+                })
 //
 //
 //                .state('app.login', {
 //                    url: '/login',
 //                    authenticate: false,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/login/app.login.html',
 //                            controller: 'LoginController'
 //                        }
@@ -90,7 +90,7 @@
 //                .state('app.logout', {
 //                    url: '/logout',
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/partials/login.html',
 //                            controller: 'logoutCtrl'
 //                        }
@@ -99,7 +99,7 @@
 //                    url: '/dashboard',
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/partials/dashboard.html',
 //                            controller: 'authCtrl'
 //                        }
@@ -107,7 +107,7 @@
 //                .state('app.admin', {
 //                    url: '/admin',
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'app.admin/app.admin.html',
 //                            controller: 'AdminController'
 //                        }
@@ -117,7 +117,7 @@
 //                    url: '/user',
 //                    //authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/app.users.html',
 //                            controller: 'UserSettingsController'
 //                        }
@@ -126,7 +126,7 @@
 //                .state('app.zeemaps', {
 //                    url: '/zeemaps',
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'testmaps/app.testmaps.html',
 //                            controller: 'MapCtrl'
 //                        }
@@ -136,7 +136,7 @@
 //                    url: '/signup',
 //                    authenticate: false,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/signup/app.signup.html',
 //                            controller: 'SignUpController'
 //                        }
@@ -146,7 +146,7 @@
 //                    url: '/userprofilesettings',
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'users/profilesettings/app.users.profilesettings.html',
 //                            controller: 'UserProfilesettings'
 //                        }
@@ -156,7 +156,7 @@
 //                    url: "/loadsheddinglanding",
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: "app.core/common/app.schedules.landing.html",
 //                            controller: 'GMapsController'
 //                        }
@@ -167,7 +167,7 @@
 //                    url: "/provinces",
 //                    //authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: 'provinces/app.provinces.html',
 //                            controller: 'ProvincesController'
 //                        }
@@ -177,7 +177,7 @@
 //                    url: "/municipalities/:province_id",
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: "municipalities/app.municipalities.html",
 //                            controller: 'MunicipalitiesController'
 //                        }
@@ -187,7 +187,7 @@
 //                    url: "/suburbs/:municipal_id",
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: "suburbs/app.suburbs.html",
 //                            controller: 'SuburbsController'
 //                        }
@@ -197,13 +197,13 @@
 //                    url: "/schedules/:suburb_id",
 //                    authenticate: true,
 //                    views: {
-//                        'app-default-view': {
+//                        'app-main': {
 //                            templateUrl: "schedules/app.schedules.html",
 //                            controller: 'SchedulesController'
 //                        }
 //                    }
 //                })
-        ;
+                ;
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/app/home');
     }
